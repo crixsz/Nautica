c2e473616571c19253c469a38663d63a3bbf71cb
crixsz <xenonzox@gmail.com>
2025-03-16 00:04:47 +0800

    refactor: remove unused variables and constants from _worker.js

--- _worker.js
+++ _worker.js
@@ -1,23 +1,12 @@
 import { connect } from "cloudflare:sockets";
 
 // Variables
-const rootDomain = "xenonzox.workers.dev"; // Ganti dengan domain utama kalian
-const serviceName = "custom"; // Ganti dengan nama workers kalian
-const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
-const apiEmail = ""; // Ganti dengan email yang kalian gunakan
-const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
-const zoneID = ""; // Ganti dengan Zone ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
-let isApiReady = false;
 let proxyIP = "";
-let cachedProxyList = [];
 
 // Constant
-const APP_DOMAIN = `${serviceName}.${rootDomain}`;
 const DNS_SERVER_ADDRESS = "8.8.8.8";
 const DNS_SERVER_PORT = 53;
 const PROXY_HEALTH_CHECK_API = "";
-const CONVERTER_URL = "";
-const DONATE_LINK = "https://trakteer.id/dickymuliafiqri/tip";
 const WS_READY_STATE_OPEN = 1;
 const WS_READY_STATE_CLOSING = 2;
 const CORS_HEADER_OPTIONS = {
